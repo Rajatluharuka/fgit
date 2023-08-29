52b32b7004ad501726d6688d801765b433dab648
Rajatluharuka <rajatluharuka123>
2023-08-29 14:25:15 +0530

    updated

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('Hi Git');
+let header=document.getElementById('main-header');
+header.style.borderBottom='solid 4px #000';
+let addItem=document.getElementById('addItem');
+addItem.style.fontWeight='bold';
+addItem.style.color='green';
