662c2ad1ed2dad069317fff00fc4aa34f80cccb9
Rajatluharuka <rajatluharuka123>
2023-08-29 14:56:05 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let header=document.getElementById('main-header');
-header.style.borderBottom='solid 4px #000';
-let addItem=document.getElementById('addItem');
-addItem.style.fontWeight='bold';
-addItem.style.color='green';
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+} 
