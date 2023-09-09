7dea98f66147b95c121f83762154d0c8907178c0
Rajatluharuka <rajatluharuka123>
2023-09-09 18:16:30 +0530

    Task 1 updated

---
+++ dom.js
@@ -0,0 +1,10 @@
+let li=document.getElementsByTagName('li');
+for(let i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor='green';
+}
+let item=document.getElementsByClassName('list-group-item');
+for(let i=0;i<item.length;i++)
+{
+    item[i].style.backgroundColor='blue';
+}
