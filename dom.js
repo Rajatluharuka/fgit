8360a749155dab47ac7206f2891df1691c12d132
Rajatluharuka <rajatluharuka123>
2023-09-11 02:26:48 +0530

    Task 3 updated

--- dom.js
+++ dom.js
@@ -1,4 +1,7 @@
-let secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor='green';
-let thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display = 'none';
+let items=document.querySelectorAll('.list-group-item');
+items[1].style.color='green';
+let oddItems=document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<oddItems.length;i++)
+{
+    oddItems[i].style.backgroundColor='green';
+}
