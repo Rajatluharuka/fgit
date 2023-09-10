618a50c3007534abffaa2ecdb885816bd2bdcd77
Rajatluharuka <rajatluharuka123>
2023-09-11 02:04:32 +0530

    Task 2 updated

--- dom.js
+++ dom.js
@@ -1,10 +1,4 @@
-let li=document.getElementsByTagName('li');
-for(let i=0;i<li.length;i++)
-{
-    li[i].style.backgroundColor='green';
-}
-let item=document.getElementsByClassName('list-group-item');
-for(let i=0;i<item.length;i++)
-{
-    item[i].style.backgroundColor='blue';
-}
+let secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+let thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
